6d67ad134386b45e464da367b0b29a3656ea3577
MaxMillington <maxmillington89@gmail.com>
2015-08-11 10:20:13 -0600

    working on learning jQuery

--- fancycle.js
+++ fancycle.js
@@ -1,1 +1,11 @@
-$(document).ready(function(){});
+$(document).ready(function(){
+    $('#justin').click(function(){
+        var song = $('#baby'),
+            counter = song.find('#view-counter').text();
+        song.toggleClass('hidden');
+        if (song.attr('class') === 'song') {
+            counter ++;
+            $('#baby').find('#view-counter').text(counter);
+        }
+    });
+});
